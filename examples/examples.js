d1fce05f3b85d5ce35cb55c5f90bc34969a79706
Christopher Barry <me@chrisb.xyz>
2016-09-26 20:56:54 -0400

    Initial complete version
    TODO:
    -README
    -Clean up examples
    -Create Tests
    -Implement promises

--- examples/examples.js
+++ examples/examples.js
@@ -92,13 +92,13 @@ lfm.auth_getMobileSession(function(res) {
 		// 		console.log(res);
 		// 	}
 		// });
-		lfm.track_updateNowPlaying({
-			track: "My Body's Made of Crushed Little Stars",
-			artist: 'Mitski',
-			callback: function(res) {
-				console.log(res);
-			}
-		});
+		// lfm.track_updateNowPlaying({
+		// 	track: "My Body's Made of Crushed Little Stars",
+		// 	artist: 'Mitski',
+		// 	callback: function(res) {
+		// 		console.log(res);
+		// 	}
+		// });
 	}
 });
 
@@ -347,4 +347,113 @@ lfm.auth_getMobileSession(function(res) {
 // 	}
 // });
 // 
+// lfm.user_getArtistTracks({
+// 	user: 'Christo27',
+// 	artist: 'Sylvan Esso',
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
 //
+// lfm.user_getFriends({
+// 	user: 'Christo27',
+// 	recenttracks: true,
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getInfo({
+// 	//user: 'Christo27', //defaults to authenticated user
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+//
+// lfm.user_getLovedTracks({
+// 	user: 'Christo27',
+// 	limit: 5,
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getPersonalTags({
+// 	user: 'Christo27',
+// 	tag: 'folk',
+// 	taggingtype: 'album',
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getRecentTracks({
+// 	user: 'Christo27',
+// 	limit: 5,
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getTopAlbums({
+// 	user: 'Christo27',
+// 	period: '1month',
+// 	limit: 5,
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getTopArtists({
+// 	user: 'Christo27',
+// 	period: '1month',
+// 	limit: 5,
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getTopTags({
+// 	user: 'Christo27',
+// 	limit: 5,
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getTopTracks({
+// 	user: 'Christo27',
+// 	period: '1month',
+// 	limit: 5,
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getWeeklyAlbumChart({
+// 	user: 'Christo27',
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getWeeklyArtistChart({
+// 	user: 'Christo27',
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getWeeklyChartList({
+// 	user: 'Christo27',
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
+// 
+// lfm.user_getWeeklyTrackChart({
+// 	user: 'Christo27',
+// 	callback: function(res) {
+// 		console.log(res);
+// 	}
+// });
